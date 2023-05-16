034aea4b51442d2408b0f060e71147a85c325bbb
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-05-16 13:39:01 +0300

    заканчиваю первый экран

--- js/scripts.js
+++ js/scripts.js
@@ -40,42 +40,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // анимация наклона карточки 
-  let cx, cy, mouseX, mouseY, posX, posY, clientX, clientY, dx, dy, tiltx, tilty, request, radius, degree;
-
-  const firstScreen = document.querySelector('body');
-  const text = document.querySelector('.first-screen');
-
-  cx = firstScreen.offsetWidth / 2;
-  cy = firstScreen.offsetHeight / 2;
-
-  firstScreen.addEventListener('mousemove', e => {
-    clientX = e.pageX - firstScreen.offsetLeft
-    clientY = e.pageY - firstScreen.offsetTop
-
-    request = requestAnimationFrame(updateMe)
-  });
-
-  function updateMe() {
-    dx = clientX - cx
-    dy = clientY - cy
-    tiltx = dy / cy
-    tilty = dx / cx
-    radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2))
-    degree = radius * 12
-
-    // Наклоняем карточку .first-screen__text
-    gsap.to(text, 1, { transform: `rotate3d(${tiltx}, ${tilty}, 0, ${degree}deg)` })
-
-    // Придаем плавность и позволяет анимации продолжаться после окончания движения мыши
-    cancelAnimationFrame(request)
-  };
-
-  // Дополнительные настройки анимации
-  gsap.set(text, { transformOrigin: 'center center' }); // Устанавливаем точку трансформации в центр элемента
-
-  gsap.to('.card', { opacity: 1, zoom: 1, duration: .1, delay: .25 }); // Анимация уменьшения масштаба карточки
-
 });
 
 // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
